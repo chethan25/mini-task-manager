06403f3d50628e33b7f133d2ba1563389c86359c
Chethan B <chethan25.bpac@gmail.com>
2022-01-05 19:39:21 +0530

    built header ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <div>Hello, World</div>;
+  return (
+    <div className="app-container">
+      <header className="header-container">
+        <h1 className="header-title">Dashboard</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
