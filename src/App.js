938d457c55e08c213fc418e4d44d2b5f728d9304
Chethan B <chethan25.bpac@gmail.com>
2022-01-06 13:56:57 +0530

    built tasks list section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
+import { useState, useEffect } from 'react';
+
+import TasksService from './services/tasks-api';
+
 import Navbar from './components/Navbar';
 import MainSection from './components/MainSection';
 
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getTasks();
+    getUsers();
+  }, []);
+
+  const getTasks = async () => {
+    const tasksData = await TasksService.getTasksList();
+
+    setTasks(tasksData.tasks);
+  };
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    const usersData = await TasksService.getUsersList();
+
+    setUsers(usersData.users);
+    setIsLoading(false);
+  };
+
   return (
     <div className="app-container">
       <header className="header-container">
-        <h1 className="header-title">Dashboard</h1>
+        <h1 className="header-title">Task Manager</h1>
       </header>
       <div className="section-container">
         <Navbar />
-        <MainSection />
+        <MainSection tasks={tasks} users={users} isLoading={isLoading} />
       </div>
     </div>
   );
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,6 +1,10 @@
+import Loader from 'react-loader-spinner';
+import { FaRegEdit } from 'react-icons/fa';
+import { FaTrashAlt } from 'react-icons/fa';
+
 import './main-section.css';
 
-const MainSection = () => {
+const MainSection = ({ tasks, users, isLoading }) => {
   return (
     <div className="main-section">
       <div className="main-section-header-container">
@@ -13,8 +17,36 @@ const MainSection = () => {
           </button>
         </div>
       </div>
+      <div className="main-section-tasks-container">
+        <ul className="tasks-list">
+          {isLoading ? (
+            <div className="tail-spin-loader-container">
+              <Loader type="TailSpin" height={60} width={60} color="#c38d9e" />
+            </div>
+          ) : (
+            tasks.map((task) => (
+              <li key={task.id}>
+                <div className="task-container">
+                  <p className="task-message-text">{task.message}</p>
+                  <p className="task-assigned-text">
+                    {users.find((user) => user.id === task.assigned_to)
+                      ? users.find((user) => user.id === task.assigned_to).name
+                      : 'Not Assigned'}
+                  </p>
+                  <h6 className="due-date-text">
+                    {new Date(task.due_date).toDateString()}
+                  </h6>
+                  <FaRegEdit className="action-icons" />
+                  <FaTrashAlt className="action-icons" />
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
 
+// user.id === task.assigned_to;
 export default MainSection;
